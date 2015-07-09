192b399adfad5f3c91b8cb9410916029503044f9
Liquid Obsidian <liquid@liqob.com>
2015-07-09 17:26:08 +0000

    Getting the whole system working.

--- client/poller.js
+++ client/poller.js
@@ -1,5 +1,25 @@
-var socket = io();
+$(function() {
+    var socket = io();
 
-function startpoll (opts) {
-    socket.emit('chat message', $('#m').val());
-}
+    function startpoll (opts) {
+        socket.emit('startpoll', 
+            {
+                "poll": opts.poll
+            }
+        );
+    }
+    
+    $('.createpoll').click(function () {
+        if ($('#pollinfo').val() == "") {
+            return;
+        } else {
+            startpoll({
+                "poll": $('#pollinfo').val()
+            });
+        }
+    });
+    
+    socket.on('pollinfo', function (data) {
+        window.location.href = "/id/" + data.id;
+    });
+});
--- server.js
+++ server.js
@@ -2,22 +2,41 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var r = require("random-js")();
+
+var polls = [];
 
 app.use(express.static('client'));
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + "/client/index.html")
+    res.sendFile(__dirname + "/client/index.html");
 });
 
 app.get('/id/:id', function (req, res) {
+    for (var i = 0; i < polls.length; i++) {
+        if (req.params.id == polls[i].id) {
+            res.end(polls[i].poll);
+            return;
+        }
+    }
+    
     console.log("requested id: " + req.params.id);
     res.end("Sorry, " + req.params.id + " is not a valid poll id.");
 });
 
 io.on('connection', function (socket) {
-    socket.on('startpoll', function (msg) {
-        
-    })
+    socket.on('startpoll', function (data) {
+        var id = r.string(5);
+        polls.push({
+            "id": id,
+            "poll": data.poll
+        });
+        console.log("New poll: \nid: " + id + "\npoll: " + data.poll);
+        socket.emit('pollinfo', {
+            "id": id,
+            "poll": data.poll
+        });
+    });
 });
 
 // replace with your own port, this was developed on c9.
